80263f698beec963e89d078e6fa33c90084c90cc
Redowan Ahmed <redowanahmed116@gmail.com>
2025-03-26 17:24:18 +0600

    add car class with basic attributes

--- src/domain/Car.java
+++ src/domain/Car.java
@@ -1,5 +1,22 @@
 package domain;
 
 public class Car {
-    
+    private String registrationNumber;
+    private String color;
+
+    public Car (String registrationNumber, String color)
+    {
+        this.registrationNumber = registrationNumber;
+        this.color = color;
+    }
+
+    public String getRegistrationNumber()
+    {
+        return this.registrationNumber;
+    }
+
+    public String getColor ()
+    {
+        return this.color;
+    }
 }
